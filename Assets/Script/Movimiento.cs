eb90963f877144d2ab10dfa5030a075ce36cacab
pEd0Cun <sotojpedro@gmail.com>
2026-03-07 21:49:28 -0600

    uso de tilemaps

--- Assets/Script/Movimiento.cs
+++ Assets/Script/Movimiento.cs
@@ -1,42 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
- 
+
 public class Movimiento : MonoBehaviour
 {
+    [Header("Movimiento")]
     [SerializeField] private float velocidadCaminata = 4f;
+
+    [Header("Salto")]
     [SerializeField] private float alturaSalto = 4f;
-    [SerializeField] private float velInicialSalto = 2f;
+    [SerializeField] private int cantidadSaltosTotales = 2; 
     [SerializeField] private LayerMask capaDeSalto;
+
+    private int saltosRestantes;
 
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        saltosRestantes = cantidadSaltosTotales;
     }
- 
+
+    void Update()
+    {
+        // Si toca el suelo, reiniciamos saltos
+        if (EstaEnSuelo())
+        {
+            saltosRestantes = cantidadSaltosTotales;
+        }
+    }
+
     public void Moverse(float movimientoX)
     {
         rb.velocity = new Vector2(movimientoX * velocidadCaminata, rb.velocity.y);
     }
 
     public void Saltar(bool debeSaltar)
-
     {
-        if (!boxCollider.IsTouchingLayers(capaDeSalto)){ return;}
+        if (!debeSaltar) return;
 
-        if(debeSaltar)
+        // Si aún tiene saltos disponibles
+        if (saltosRestantes > 0)
         {
-            //Mejoramiento del Salto
-            //rb.velocity = new Vector2(rb.velocity.x, velInicialSalto);
-            velInicialSalto = Mathf.Sqrt(-2 * alturaSalto * Physics2D.gravity.y * rb.gravityScale);
+            float velInicialSalto = Mathf.Sqrt(-2 * alturaSalto * Physics2D.gravity.y * rb.gravityScale);
             rb.velocity = new Vector2(rb.velocity.x, velInicialSalto);
+            saltosRestantes--;
         }
     }
- 
+
+    private bool EstaEnSuelo()
+    {
+        return boxCollider.IsTouchingLayers(capaDeSalto);
+    }
 }
